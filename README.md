3682df0021216e3a8518a3ca360bcd35136d383f
Daniel Suo <danielsuo@gmail.com>
2017-08-31 16:55:01 -0700

    Add folly and wangle

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # equi
 A repository which must be renamed
+
+## TODO
+- Add folly and wangle as dependencies
+- Get Pool working and test
+- Get Blackscholes working and test several clients
+- Implement some dummy scheduling policies
---
+++ scripts/install_ubuntu.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+sudo apt-get install \
+	g++ \
+	automake \
+	autoconf \
+	autoconf-archive \
+	libtool \
+	libboost-all-dev \
+	libevent-dev \
+	libdouble-conversion-dev \
+	libgoogle-glog-dev \
+	libgflags-dev \
+	liblz4-dev \
+	liblzma-dev \
+	libsnappy-dev \
+	make \
+	zlib1g-dev \
+	binutils-dev \
+	libjemalloc-dev \
+	libssl-dev \
+	pkg-config \
+	libunwind8-dev \
+	libelf-dev \
+	libdwarf-dev \
+	libiberty-dev
